a8c436239aa7a9e5d45ba93598c4c759126ab09b
ZWF <2360690156@qq.com>
2023-10-14 11:28:30 +0800

    完成二级路由的配置

--- src/router/index.js
+++ src/router/index.js
@@ -6,12 +6,26 @@ import MyOrder from '@/views/myorder/index'
 import Pay from '@/views/pay/index'
 import ProductDetail from '@/views/productdetail/index'
 import Search from '@/views/search/index'
+import Home from '@/views/layout/home'
+import Category from '@/views/layout/category'
+import Cart from '@/views/layout/cart'
+import User from '@/views/layout/user'
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   routes: [
-    { path: '/', component: LayOut },
+    {
+      path: '/',
+      component: LayOut,
+      redirect: '/home',
+      children: [
+        { path: '/home', component: Home },
+        { path: '/category', component: Category },
+        { path: '/cart', component: Cart },
+        { path: '/user', component: User }
+      ]
+    },
     { path: '/login', component: Login },
     { path: '/myorder', component: MyOrder },
     { path: '/pay', component: Pay },
--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
     <!-- 二级路由入口 -->
-    <van-tabbar active-color="#ee0a24" inactive-color="#000">
-      <van-tabbar-item icon="home-o">首页</van-tabbar-item>
-      <van-tabbar-item icon="apps-o">分类</van-tabbar-item>
-      <van-tabbar-item icon="cart-o">购物车</van-tabbar-item>
-      <van-tabbar-item icon="contact-o">我的</van-tabbar-item>
+    <router-view></router-view>
+    <van-tabbar route active-color="#ee0a24" inactive-color="#000">
+      <van-tabbar-item to="/home" icon="home-o">首页</van-tabbar-item>
+      <van-tabbar-item to="/category" icon="apps-o">分类</van-tabbar-item>
+      <van-tabbar-item to="/cart" icon="cart-o">购物车</van-tabbar-item>
+      <van-tabbar-item to="/user" icon="contact-o">我的</van-tabbar-item>
     </van-tabbar>
   </div>
 </template>
