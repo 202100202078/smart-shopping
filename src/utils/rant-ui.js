1f6144f26626bf1b9f7f4f59a3f8e20022f40eb5
ZWF <2360690156@qq.com>
2023-10-16 16:50:32 +0800

    完成首页静态页面搭建及渲染

--- src/utils/rant-ui.js
+++ src/utils/rant-ui.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
-import { Toast, Rate, Tabbar, TabbarItem, NavBar } from 'vant'
+import { Search, Swipe, SwipeItem, Grid, GridItem, Toast, Rate, Tabbar, TabbarItem, NavBar } from 'vant'
 
 Vue.use(Toast)
 Vue.use(Tabbar)
 Vue.use(NavBar)
 Vue.use(TabbarItem)
 Vue.use(Rate)
+Vue.use(GridItem)
+Vue.use(Search)
+Vue.use(Swipe)
+Vue.use(SwipeItem)
+Vue.use(Grid)
