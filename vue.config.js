7bce0b3d40a7e2ae9fd431998bd9414a5861649e
ZWF <2360690156@qq.com>
2023-10-21 13:49:07 +0800

    修改配置项

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   publicPath: './',
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    open: true, // 编译后默认打开浏览器
+    historyApiFallback: true,
+    allowedHosts: 'all'
+  }
 })
